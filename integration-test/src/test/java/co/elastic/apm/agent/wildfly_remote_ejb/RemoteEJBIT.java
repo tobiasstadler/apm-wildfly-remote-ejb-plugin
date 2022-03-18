d4c0a1ea63b4f1526abf2bf55a36f90cd15efde8
Tobias Stadler <ts.stadler@gmx.de>
2022-03-18 08:58:40 +0100

    Updated mock-server to 5.13.0

--- integration-test/src/test/java/co/elastic/apm/agent/wildfly_remote_ejb/RemoteEJBIT.java
+++ integration-test/src/test/java/co/elastic/apm/agent/wildfly_remote_ejb/RemoteEJBIT.java
@@ -60,7 +60,7 @@ class RemoteEJBIT {
     private static final Network NETWORK = Network.newNetwork();
 
     @Container
-    private static final GenericContainer<?> MOCK_SERVER = new GenericContainer<>(DockerImageName.parse("mockserver/mockserver:mockserver-5.12.0"))
+    private static final GenericContainer<?> MOCK_SERVER = new GenericContainer<>(DockerImageName.parse("mockserver/mockserver:mockserver-5.13.0"))
             .withNetwork(NETWORK)
             .withNetworkAliases("apm-server")
             .withExposedPorts(1080)
